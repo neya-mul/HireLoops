7d861b67d43a03d06591cedf274a9c5666a1022b
neya-mul <neyamulislam946@gmail.com>
2026-05-31 20:13:24 +0600

    sign in page added

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -87,7 +87,7 @@ export default function Navbar() {
                         {/* Action Buttons */}
                         <div className="flex items-center gap-4 text-sm">
                             <Link
-                                href="#"
+                                href="signin"
                                 className="text-[#6366f1] hover:text-[#5053f0] font-semibold transition-colors duration-200"
                             >
                                 Sign In
